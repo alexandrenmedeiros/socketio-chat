5d48b43634de2aff222fe847fb486ff17cf6f1a2
Alexandre Medeiros <xandynorcia@gmail.com>
2021-01-28 20:43:44 -0300

    trying to use socket

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
-asdfasdfsas
+const express = require('express')
+const path = require('path')
+const http = require('http')
+const io = require('socket.io')(http)
 
-klkskskdfa
+const app = express()
+const server = http.createServer(app)
+
+const PORT = 3000 || process.env.PORT
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+io.on('connection', (socket) => {
+    console.log(`new socket connected: ${socket.id}`)
+})
+
+server.listen(PORT, () => {
+    console.log(`listen on port ${PORT}`)
+})
