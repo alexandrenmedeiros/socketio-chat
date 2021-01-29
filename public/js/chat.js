0f29e0bafef6cb90d3471b3709b5d91f9f844ba0
Alexandre Medeiros <xandynorcia@gmail.com>
2021-01-29 20:40:27 -0300

    users array (not done)

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,11 @@
 const socket = io()
+
+var join = document.getElementById('join_chat')
+
+// get username on connection
+join.addEventListener('submit', (e) => {
+    // e.preventDefault() // sem prevent pra poder mudar de pagina
+
+    const username = e.target.elements.username.value
+    socket.emit('new_user', username)
+})
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// includes and global variables 
 const express = require('express')
 const path = require('path')
 const http = require('http')
@@ -11,8 +12,32 @@ const PORT = 3000 || process.env.PORT
 
 app.use(express.static(path.join(__dirname, 'public')))
 
+var users = []
+
+// functions
+function delete_id(id) {
+    for (var i = users.length - 1; i >= 0; i--) {
+        if (users[i].id === id) {
+         users.splice(i, 1);
+        }
+    }
+}
+
+// MAIN
+
 io.on('connection', (socket) => {
     console.log(`new socket connected: ${socket.id}`)
+
+    socket.on('new_user', (username) => {
+        console.log(username)
+        users.push({username:username, id:socket.id})
+    })
+
+
+    socket.on('disconnect', () => {
+        delete_id(socket.id)
+        console.log(`user ${socket.id} has disconnect`)
+    })
 })
 
 server.listen(PORT, () => {
