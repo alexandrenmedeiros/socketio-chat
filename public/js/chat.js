d3a37c763098fc786a30e9fc88c60c2deef0f2e3
Alexandre Medeiros <xandynorcia@gmail.com>
2021-01-30 14:38:46 -0300

    change some style

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,17 +1,20 @@
 const socket = io()
 
 const message = document.getElementById('send_message')
-const chat = document.getElementById('messages_list')
+const chat = document.getElementById('chat')
+const msg_list = document.getElementById('messages_list')
 
 const user = Qs.parse(location.search, {
     ignoreQueryPrefix: true
 })
 
 
-function format_msg(username, content) {
+function format_msg(username, content, h, min) {
     return {
         user: username,
-        msg: content
+        msg: content,
+        h: h,
+        min: min,
     }
 }
 
@@ -19,15 +22,22 @@ message.addEventListener('submit', (e) => {
     e.preventDefault()
 
     let msg = e.target.elements.message.value
-    e.target.elements.message.value = ''
-
+    let time = new Date()
+    
     // console.log(format_msg(user.username, msg))
-    socket.emit('user_message', format_msg(user.username, msg))
+    socket.emit('user_message', format_msg(user.username, msg, time.getHours(), time.getMinutes()))
+    
+    e.target.elements.message.value = ''
+    e.target.elements.message.focus();
 })
 
 socket.on('new_message', (e) => {
     // console.log(e)
     let new_msg = document.createElement('li')
-    new_msg.innerHTML = `<strong>${e.user}</strong>: ${e.msg}`
-    chat.appendChild(new_msg);
+
+    new_msg.innerHTML = `<strong>${e.h}:${e.min} ${e.user}</strong>: ${e.msg}`
+    new_msg.classList.add('list-group-item')
+    
+    msg_list.appendChild(new_msg);
+    chat.scrollTop = chat.scrollHeight;
 })
