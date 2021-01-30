5693c598b906615b4058b24db44dd7246088398b
Alexandre Medeiros <xandynorcia@gmail.com>
2021-01-30 13:42:42 -0300

    basic chat done

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,11 +1,33 @@
 const socket = io()
 
-var join = document.getElementById('join_chat')
+const message = document.getElementById('send_message')
+const chat = document.getElementById('messages_list')
 
-// get username on connection
-join.addEventListener('submit', (e) => {
-    // e.preventDefault() // sem prevent pra poder mudar de pagina
+const user = Qs.parse(location.search, {
+    ignoreQueryPrefix: true
+})
 
-    const username = e.target.elements.username.value
-    socket.emit('new_user', username)
+
+function format_msg(username, content) {
+    return {
+        user: username,
+        msg: content
+    }
+}
+
+message.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    let msg = e.target.elements.message.value
+    e.target.elements.message.value = ''
+
+    // console.log(format_msg(user.username, msg))
+    socket.emit('user_message', format_msg(user.username, msg))
+})
+
+socket.on('new_message', (e) => {
+    // console.log(e)
+    let new_msg = document.createElement('li')
+    new_msg.innerHTML = `<strong>${e.user}</strong>: ${e.msg}`
+    chat.appendChild(new_msg);
 })
--- server.js
+++ server.js
@@ -12,30 +12,17 @@ const PORT = 3000 || process.env.PORT
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-var users = []
-
-// functions
-function delete_id(id) {
-    for (var i = users.length - 1; i >= 0; i--) {
-        if (users[i].id === id) {
-         users.splice(i, 1);
-        }
-    }
-}
-
 // MAIN
-
 io.on('connection', (socket) => {
     console.log(`new socket connected: ${socket.id}`)
 
-    socket.on('new_user', (username) => {
-        console.log(username)
-        users.push({username:username, id:socket.id})
+    socket.on('user_message', (e) => {
+        console.log(e)
+        io.emit('new_message', e)
     })
 
 
     socket.on('disconnect', () => {
-        delete_id(socket.id)
         console.log(`user ${socket.id} has disconnect`)
     })
 })
